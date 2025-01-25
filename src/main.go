9a669190d8fea0b0102e525bdebf106d707005ad
AnnWann <uanabgasparim@gmail.com>
2025-01-25 12:16:11 -0300

    refactor: main

---
+++ src/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"os"
+
+	"github.com/AnnWann/pstu_finance_system/src/database"
+	"github.com/AnnWann/pstu_finance_system/src/parser"
+	"github.com/AnnWann/pstu_finance_system/src/views/terminal"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	file := os.Getenv("DB_FILE")
+	database.GetDB().InitDB(file)
+	reader := bufio.NewReader(os.Stdin)
+	var input string
+
+	for {
+		input, _ = reader.ReadString('\n')
+		command, modifiers, arguments := parser.Parse(input)
+
+		terminal.HandleOption(command, modifiers, arguments)
+	}
+
+}
+
